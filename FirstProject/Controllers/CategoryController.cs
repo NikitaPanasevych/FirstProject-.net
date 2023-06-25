736c8a0613a53f9c87eb29ff9458d63de595a0b7
DeStRaaM <20destraam03@gmail.com>
2023-06-25 16:18:19 +0200

    CRUD operations

---
+++ FirstProject/Controllers/CategoryController.cs
@@ -0,0 +1,115 @@
+﻿using FirstProject.Controllers.Data;
+using FirstProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstProject.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        //READ
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+
+        //GET Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category created successfuly";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //GET Edit
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            else {
+                var categoryFromDb = _db.Categories.Find(id);
+                if (categoryFromDb == null)
+                {
+                    return NotFound();
+                }
+                return View(categoryFromDb);
+            }
+            
+        }
+
+        //POST Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category updated successfuly";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //GET Delete
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            else
+            {
+                var categoryFromDb = _db.Categories.Find(id);
+                if (categoryFromDb == null)
+                {
+                    return NotFound();
+                }
+                return View(categoryFromDb);
+            }
+
+        }
+
+        //POST Delete
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+                var obj = _db.Categories.Find(id);
+                if (obj == null)
+                {
+                    return NotFound();
+                }
+
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category deleted successfuly";
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
